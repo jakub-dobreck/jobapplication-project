89505a6614603f4229ff64db931764345bb6c2d4
jakub-dobreck <jakub.dobreck@gmail.com>
2019-02-01 20:51:10 +0100

    Create a job application candidate model

--- jobapp_form/models.py
+++ jobapp_form/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Candidate(models.Model):
+    first_name = models.CharField(max_length = 35)
+    last_name = models.CharField(max_length = 35)
+    picture_url = models.CharField(max_length = 50)
+    cv_url = models.CharField(max_length = 50)
+    date_time = models.DateTimeField
+
+    def __str__(self):
+        return(self.first_name + " " + self.last_name)
+
+    def date(self):
+        return(self.date_time.strftime('%d/%m/%Y'))
+
+    def time(self):
+        return(self.date_time.strftime('%H:%M'))
